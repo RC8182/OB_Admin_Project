5781920dcf2f6d918a697013708aa455eba60ad7
Javier Visconti <javiervisconti7@gmail.com>
2022-06-11 18:32:09 +0100

    Sign in google form

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import React from 'react';
 import './App.css';
-import { ChakraProvider } from '@chakra-ui/react'
+
 //import FormRegister from './shared/components/pure/forms/formRegister';
 import FormLogin from './shared/components/pure/forms/formLogin';
 
@@ -10,15 +10,15 @@ function App() {
   return (
     
     <div className="App">
-        <ChakraProvider>
+
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
                   Proyecto Admin OpenBootCamp !!!
-                 {/*<FormLogin />*/}
+                 {<FormLogin />}
                 </p>
                 </header>      
-        </ChakraProvider>
+
     </div>    
 
 
--- src/shared/components/pure/forms/formLogin.jsx
+++ src/shared/components/pure/forms/formLogin.jsx
@@ -1,4 +1,6 @@
+import { Input, Stack, Button } from '@chakra-ui/react';
 import React, { useState } from 'react';
+import GoogleLogin from 'react-google-login';
 
 const LoginForm = () => {
 
@@ -6,12 +8,34 @@ const LoginForm = () => {
 
     const [credentials , setCredentials] = useState(initialCredentials)
 
-    return (
-        <div>
-            <h3>
+    const responseGoogle=(response)=>{console.log(response)/* Creamos la funcion para la respuesta de Google */} 
 
-            </h3>
-        </div>
+    return (
+                    
+        <form>
+              <h1> Login with Google </h1>
+              <br/>
+              <GoogleLogin id='g'
+                clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+                buttonText="Sign in whit Google "
+                onSuccess={responseGoogle}
+                onFailure={responseGoogle}
+                cookiePolicy={'single_host_origin'}/>
+                <br/>
+
+               <h1 id='separador'> or </h1> 
+               
+            <Stack spacing={3}>
+                <h5 align='left'>Email* </h5>
+                <Input placeholder='mail@example.com'  />
+
+                <h5 align='left'>Password* </h5>    
+                <Input placeholder='Min. 8 characters' />
+
+                <Button type='submit' color={'white'} backgroundColor='blue' > Sign In </Button>
+
+            </Stack>
+        </form>
     );
 }
 
